6b056dbb466960a8c53badda5d1fa8a717e46f88
cecuser <cecuser@cec2020>
2020-02-20 09:12:36 +0200

    update

---
+++ run.sh
@@ -0,0 +1,5 @@
+sudo docker-compose --compatibility down
+git pull https://github.com/wald3r/cc_virtualizationProject.git
+sudo docker-compose --compatibility build
+sudo docker-compose --compatibility up  -d --scale server=3 --scale redis=1
+cd balancer && npm start
