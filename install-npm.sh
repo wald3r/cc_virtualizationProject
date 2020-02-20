55dcabc938292dc863c73779b8455ec62b14b5a3
cecuser <cecuser@cec2020>
2020-02-20 09:27:53 +0200

    update

---
+++ install-npm.sh
@@ -0,0 +1,3 @@
+sudo apt-get -y install nodejs 
+sudo apt-get -y install npm 
+
--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@ sudo docker-compose --compatibility down
 git pull https://github.com/wald3r/cc_virtualizationProject.git
 sudo docker-compose --compatibility build
 sudo docker-compose --compatibility up  -d --scale server=3 --scale redis=1
-cd balancer && npm start
+
+cd balancer && npm install && npm update && npm start
